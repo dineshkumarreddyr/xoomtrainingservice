2395a9b588f4c5d795be6b22bac6c9c5d2d76a3f
dineshkumarreddyr <dinesh.rachumalla@hotmail.com>
2015-05-13 17:20:09 +0530

    1. Create a node js structure 2. Created rest services for home screen

---
+++ app.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+//Referring the routes
+var home = require('./routes/home');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Adding the Restful API
+app.post('/signup',home.signup);
+app.post('/signin',home.signin);
+app.get('/courselist',home.getcourselist);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.listen(9545);
+if(app.get('env')==='development'){
+    console.log("App listening at port 9545");
+}
+
+module.exports = app;
---
+++ routes/home.js
@@ -0,0 +1,80 @@
+/**
+ * Created by BOON on 09-05-2015.
+ */
+var express = require('express'),
+    mysql = require('mysql');
+
+var app = express();
+
+var dbConfig = {
+    host: 'localhost',
+    user: 'sa',
+    password: '1234'
+};
+
+exports.signup = function (req,res) {
+    var data = req.body;
+    try {
+        if (data != undefined) {
+            var connection = mysql.createConnection(dbConfig);
+            connection.connect();
+            connection.query("CALL xoomtrainings.SP_XTUSERS('" + data.firstname + "','" + data.lastname + "','" + data.phonenumber + "','" +
+                data.userindian + "','" + data.country + "','" + data.username + "','" + data.email + "','" + data.userpassword + "','i');",
+                function (err, rows) {
+                    if (!err)
+                        res.send('Success');
+                    else if (err.errno != undefined && err.errno === 1062) {
+                        res.send({"status": "error", "ecode": "e1", "emsg": "User already exists"});
+                    }
+                    else
+                        res.send(err);
+
+                });
+            connection.end();
+        }
+    }
+    catch (exception) {
+        console.log(exception);
+    }
+};
+
+exports.signin = function (req,res) {
+    var data = req.body;
+    try {
+        if (data != undefined && data.username != undefined && data.userpassword != undefined) {
+            var connection = mysql.createConnection(dbConfig);
+            connection.connect();
+            connection.query("CALL xoomtrainings.SP_LOGIN('" + data.username + "','" + data.userpassword + "');", function (err, rows) {
+                if (!err) {
+                    if (rows != undefined && rows[0] != undefined && rows[0].length > 0) {
+                        res.send({"status": "success", "records": rows[0]});
+                    }
+                    else
+                        res.send({"status": "error", "ecode": "d2", "emsg": "Data does not exist"});
+                }
+                else
+                    res.send({"status": "error", "ecode": "d3", "emsg": err.message});
+            });
+            connection.end();
+        }
+    }
+    catch (exception) {
+        console.log(exception);
+    }
+};
+
+//Get List of courses
+exports.getcourselist = function(req,res) {
+    try {
+        var connection = mysql.createConnection(dbConfig);
+        connection.connect();
+        connection.query('CALL `xoomtrainings`.`SP_GETCOURSES`();', function (error, response) {
+            if (!error) {
+                res.send({"status": "success", "records": response[0]});
+            }
+        });
+    }
+    catch (exception) {
+        console.log(exception);
+    }
+};
