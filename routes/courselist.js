af4dc238c99e9c99904f96b09cfb0c5537e22ef2
dineshkumarreddyr <dinesh.rachumalla@hotmail.com>
2015-05-19 15:06:15 +0530

    1. API for course and uploading the images of course to server

---
+++ routes/courselist.js
@@ -0,0 +1,26 @@
+﻿/**
+ * Created by BOON on 19-05-2015.
+ */
+var express = require('express'),
+    mysql = require('mysql'),
+    config = require('../globals/config');
+
+var app = express();
+
+
+exports.getcourselist = function (req, res) {
+    try {
+        var connection = mysql.createConnection(config.module.dbConfig);
+        connection.connect();
+        connection.query('CALL xoomtrainings.SP_GETCOURSES();', function (err, records) {
+            console.log(err);
+            if (!err) {
+                res.send({ "status": "success", "records": records[0] });
+            }
+        });
+        connection.close();
+    }
+    catch (e) {
+        console.log(e.message);
+    }
+}
